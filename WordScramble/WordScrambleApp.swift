f2daeba17137374a4bc138d47a953227c2a7b0fd
Aitzaz Munir Shaikh <ams845@cornell.edugit config --global user.email ams845@cornell.edu>
2024-07-03 23:58:33 +0500

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Aitzaz Munir on 03/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
